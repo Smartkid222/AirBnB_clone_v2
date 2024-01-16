0383c1312b68da07c717d43df7857c95cc1837d0
Smartkid222 <emekam848@gmail.com>
2024-01-16 13:08:14 -0800

    state

--- models/state.py
+++ models/state.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
 import models
 from os import getenv
 from models.base_model import Base
+from models.base_model import BaseModel
 from models.city import City
 from sqlalchemy import Column
 from sqlalchemy import String
@@ -11,7 +11,16 @@ from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """Represents a state for a MySQL database.
+
+    Inherits from SQLAlchemy Base and links to the MySQL table states.
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store States.
+        name (sqlalchemy String): The name of the State.
+        cities (sqlalchemy relationship): The State-City relationship.
+    """
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship("City",  backref="state", cascade="delete")
 
