547648fc5879ac4f7c98f3cfee631d5c1d8ab610
Enioluwayan7 <olabanjienioluwayan@gmail.com>
2024-01-17 14:32:56 -0800

    update

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""This is the amenity class"""
+"""This is the amenity class update"""
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
 from models.place import place_amenity
-
+from sqlalchemy import Column, String
 
 class Amenity(BaseModel, Base):
+    """Class for Amenity
+    Attributes:
+        name: input name
+    """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place", secondary=place_amenity)
+
+
